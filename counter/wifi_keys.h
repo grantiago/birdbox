3fb9fd41cc135a2733b8376df5d4bcb781bb7c50
grant amaral <grantamaral@gmail.com>
2023-05-01 14:53:23 -0600

    Create wifi_keys.h

---
+++ counter/wifi_keys.h
@@ -0,0 +1,7 @@
+const char* ssid = "myWifi";  // replace all
+const char* password = "myPass";
+//String serverName = "192.168.1.21";
+//String serverName = "grant.lan";   // REPLACE WITH YOUR Raspberry Pi IP ADDRESS
+//String serverPath = "/birdbox/upload.php";     // The default serverPath should be upload.php
+const int serverPort = 80;
+//const int serverPort = 443;
